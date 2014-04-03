5b9c1233e739accdb9b10bebc19589244539666a
Francesco <frosso@users.noreply.github.com>
2014-04-03 17:25:03 +0200

    Rename variables for readability; Fix attachments bug;

--- frosso_task_to_comment/controllers/FrossoTaskToCommentController.class.php
+++ frosso_task_to_comment/controllers/FrossoTaskToCommentController.class.php
@@ -25,112 +25,121 @@ class FrossoTaskToCommentController extends ProjectController {
     /**
      * Prepare controller
      */
-    function __before( ) {
-        parent::__before( );
+    function __before() {
+        parent::__before();
 
         $task_id = $this->request->getId( 'task_id' );
         if ( $task_id )
             $this->active_task = Tasks::findByTaskId( $this->active_project, $task_id );
     }
 
-    function to_comment( ) {
+    function to_comment() {
         $tasks = Tasks::findForObjectsList( $this->active_project, $this->logged_user );
 
-        $post = $this->request->post( );
+        $post = $this->request->post();
         if ( !empty( $post ) ) {
 
-            $main_task = Tasks::findByTaskId( $this->active_project, $post['merge_to_task'] );
+            $destination_task = Tasks::findByTaskId( $this->active_project, $post['merge_to_task'] );
 
-            if ( $main_task ) {
+            if ( $destination_task ) {
 
                 // creo il commento con lo stesso testo
-                $old_body = $this->active_task->getBody( );
-                $comment_body = "<h2>Merged from task " . $this->active_task->getName( ) . "</h2><br />";
-                $comment_body = $comment_body . "<small>Moved by " . $this->logged_user->getName( ) . "</small><br />" . $old_body;
+                $old_body = $this->active_task->getBody();
+                $comment_body = "<h2>Merged from task " . $this->active_task->getName() . "</h2><br />";
+                $comment_body = $comment_body . "<small>Moved by " . $this->logged_user->getName() . "</small><br />" . $old_body;
 
-                $new_comment = $main_task->comments( )->newComment( );
-                $new_comment->setBody( $comment_body );
-                $new_comment->setCreatedBy( $this->active_task->getCreatedBy( ) );
-                $new_comment->setState( STATE_VISIBLE );
+                $destination_comment = $destination_task->comments()->newComment();
+                $destination_comment->setBody( $comment_body );
+                $destination_comment->setCreatedBy( $this->active_task->getCreatedBy() );
+                $destination_comment->setState( STATE_VISIBLE );
 
-                $new_comment->setCreatedOn( DateTimeValue::now( ) );
-                
+                $destination_comment->setCreatedOn( DateTimeValue::now() );
+
                 // copio l'orario di creazione originale
                 if ( $post['options']['mantain_time'] == '1' ) {
-                    $new_comment->setCreatedOn( $this->active_task->getCreatedOn( ) );
+                    $destination_comment->setCreatedOn( $this->active_task->getCreatedOn() );
                 }
 
                 // TODO: copiare i subscribers
-                if ( isset($post['options']['copy_subscribers']) && $post['options']['copy_subscribers'] == '1' ) {
-                    $active_task_subscriptions = (array)$this->active_task->subscriptions( )->get( );
-                    $main_task_subscriptions = (array)$main_task->subscriptions( )->get( );
+                if ( isset( $post['options']['copy_subscribers'] ) && $post['options']['copy_subscribers'] == '1' ) {
+                    $active_task_subscriptions = (array)$this->active_task->subscriptions()->get();
+                    $main_task_subscriptions = (array)$destination_task->subscriptions()->get();
 
                     foreach ( $active_task_subscriptions as $key => $active_subscription_user ) {
                         foreach ( $main_task_subscriptions as $main_task_subscription_user ) {
-                            if ( $active_subscription_user->getId( ) == $main_task_subscription_user->getId( ) ) {
+                            if ( $active_subscription_user->getId() == $main_task_subscription_user->getId() ) {
                                 unset( $active_task_subscriptions[$key] );
                                 break;
                             }
                         }
                     }
-                    $active_task_subscriptions = array_merge( $active_subscriptions, $main_task_subscriptions );
-                    if ( !empty( $active_subscriptions ) ) {
-                        $main_task->subscriptions( )->set( $active_subscriptions );
+                    $active_task_subscriptions = array_merge( $active_task_subscriptions, $main_task_subscriptions );
+                    if ( !empty( $active_task_subscriptions ) ) {
+                        $destination_task->subscriptions()->set( $active_task_subscriptions );
                     }
                 }
                 // FINE
 
                 // copio i time records nel task principale
                 if ( $post['options']['timesheets'] == '1' ) {
-                    $merger_task_time_records = TimeRecords::find( array( 'conditions' => array(
+                    $merger_task_time_records = TimeRecords::find( array(
+                        'conditions' => array(
                             'parent_type = ? AND parent_id = ?',
                             'Task',
-                            $this->active_task->getId( )
-                        ) ) );
+                            $this->active_task->getId()
+                        )
+                    ) );
 
                     foreach ( $merger_task_time_records as $time_record ) {
-                        $copy_time_record = $time_record->copy( );
-                        $copy_time_record->setParentId( $main_task->getId( ) );
-                        $new_summary = $copy_time_record->getSummary( ) . " (from task " . $this->active_task->getName( ) . ")";
+                        $copy_time_record = $time_record->copy();
+                        $copy_time_record->setParentId( $destination_task->getId() );
+                        $new_summary = $copy_time_record->getSummary() . " (from task " . $this->active_task->getName() . ")";
                         $copy_time_record->setSummary( $new_summary );
-                        $copy_time_record->save( );
+                        $copy_time_record->save();
                     }
+                }
+
+                // salvare commento
+                $destination_comment->save();
+                // salvare main_task
+                $destination_task->save();
+                // salvo vecchio task
+                /** @var $this ->active_task Task */
+                $this->active_task->save();
+
+                // cloniamo gli allegati dopo aver salvato
+                if ( $post['options']['attachments'] == '1' ) {
+                    $this->active_task->attachments()->cloneTo( $destination_comment );
+                    foreach ( $destination_comment->attachments()->get( $this->logged_user ) as $attachment ) {
+                        /** @var $attachment IAttachmentsImplementation */
+                        $attachment->setState( STATE_VISIBLE );
+                    }
+                    $destination_comment->save();
+                    $destination_task->save();
                 }
 
                 // sposto nel cestino
                 if ( $post['options']['to_trash'] == '1' ) {
-                    $this->active_task->state( )->trash( );
+                    $this->active_task->state()->trash();
+                    $this->active_task->save();
                 }
 
-                // salvare commento
-                $new_comment->save( );
-                // salvare main_task
-                $main_task->save( );
-                // salvo vecchio task
-                $this->active_task->save( );
+                $this->response->redirectToUrl( $destination_task->getViewUrl() );
 
-                // cloniamo gli allegati dopo aver salvato
-                if ( $post['options']['attachments'] == '1' ) {
-                    $this->active_task->attachments( )->cloneTo( $new_comment );
-                    foreach ( $new_comment->attachments() as $attachment ) {
-                        $attachment->setState( STATE_VISIBLE );
-                    }
-                }
-
-                $this->response->redirectToUrl( $main_task->getViewUrl( ) );
                 return;
 
             } // if main_task
 
-        }// if !empty(post)
+        }
+        // if !empty(post)
 
         $this->response->assign( array(
-            'url' => Router::assemble( 'task_to_comment', array(
-                'project_slug' => $this->active_task->getProject( )->getSlug( ),
-                'task_id' => $this->active_task->getTaskId( )
-            ) ),
+            'url'       => Router::assemble( 'task_to_comment', array(
+                    'project_slug' => $this->active_task->getProject()->getSlug(),
+                    'task_id'      => $this->active_task->getTaskId()
+                ) ),
             'main_task' => $this->active_task,
-            'tasks' => $tasks,
+            'tasks'     => $tasks,
         ) );
     }
 
